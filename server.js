bcd516324054d9ddf28702d369b08d8e91f1dab8
Ruchishil-Manushera <sudipdas1022@gmail.com>
2024-12-03 17:45:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Disable cache for all requests
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
   next();
@@ -16,16 +17,12 @@ app.use((req, res, next) => {
   const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
   const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
 
-  // Disable cache
-  res.setHeader('Cache-Control', 'no-store');
-
   if (login && password && login === auth.login && password === auth.password) {
     return next();
   }
   res.set('WWW-Authenticate', 'Basic realm="Protected Area"');
   res.status(401).send('Authentication required.');
 });
-
 
 // Serve static files (HTML, CSS, etc.)
 app.use(express.static('public'));
