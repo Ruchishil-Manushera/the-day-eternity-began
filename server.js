de3eecbecd922c97bdad451de568a8e7558db3ae
Ruchishil-Manushera <sudipdas1022@gmail.com>
2024-12-03 14:27:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true })); // To parse form data
+app.use(express.static('public')); // Serve static files (HTML, CSS, etc.)
+
+// Password middleware
+app.use((req, res, next) => {
+  const auth = { login: 'teddy.of.panda.320', password: '04Nv2@@6Ankita' }; // Change these values
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  if (login && password && login === auth.login && password === auth.password) {
+    return next(); // Access granted
+  }
+  res.set('WWW-Authenticate', 'Basic realm="Protected Area"');
+  res.status(401).send('Authentication required.');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); // Load your homepage
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
