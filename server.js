c23503198747ddc1771601b90a257454fcd3192c
Ruchishil-Manushera <sudipdas1022@gmail.com>
2025-01-02 17:42:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const auth = { login: process.env.AUTH_USERNAME || 'admin', password: process.env.AUTH_PASSWORD || 'password' };
 
